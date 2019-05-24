7d9b4192b5b875236c54ff797d96bbc55fef79bb
Carly Pecora <carly@carlypecora.com>
2019-05-24 16:11:17 -0400

    add api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+	helper_method :get_api_request
+
+	def get_api_request(landmark)
+		RestClient.get("https://en.wikipedia.org/w/api.php?action=opensearch&search=#{landmark}i&limit=10&namespace=0&format=json")
+	end
+
 end
