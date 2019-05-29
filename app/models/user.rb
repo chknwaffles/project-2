722226152b4a1b9a0a1e72a633012b7228308f7d
Won Kim <wonsae.kim@gmail.com>
2019-05-29 09:08:55 -0400

    add ratings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :ratings
     # validates :password, length: {minimum: 6}
+    validates :username, uniqueness: true
     has_secure_password
     # def password
     #     @password
