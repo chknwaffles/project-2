48810ef1dfe8664f20c736eaff4b9f1da315f01f
Won Kim <wonsae.kim@gmail.com>
2019-05-29 16:27:02 -0400

    done

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,10 @@
 class ArtistsController < ApplicationController
     def search_artist
         artist_hash = RSpotify::Artist.search(params[:search])
+        if artist_hash == nil
+
+        end
+        
         @artist = Artist.find_or_create_by(name: artist_hash.first.name, spotify_id: artist_hash.first.id, image_url: artist_hash.first.images[1]["url"])
         @artist.create_all(artist_hash.first)
         redirect_to @artist
