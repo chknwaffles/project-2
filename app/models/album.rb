4fe29659f83cceed77da3553e8ca4bac17ddb247
Won Kim <wonsae.kim@gmail.com>
2019-05-29 13:04:31 -0400

    fix small issues

--- app/models/album.rb
+++ app/models/album.rb
@@ -7,6 +7,6 @@ class Album < ApplicationRecord
     end
 
     def self.top_rated_albums
-        self.all.select {|album| album.album_rating if !album.album_rating.nan? }.sort_by{|album| album.average_rating }.reverse
+        self.all.select {|album| album.album_rating if !album.album_rating.nan? }.sort_by{|album| album.album_rating }.reverse
     end
 end
